fc7a41fd7990ed48d01361c9d1a2ffff33e59cf0
RedMattis <redmattis@gmail.com>
2024-08-30 23:03:41 +0200

    Graphics (code or art) mostly.

--- 1.5/Source/Oops Bugs/Rendering.cs
+++ 1.5/Source/Oops Bugs/Rendering.cs
@@ -9,6 +9,24 @@ using Verse;
 
 namespace OopsBug
 {
+    public class HSVColor
+    {
+        public float? hue = null;
+        public float? saturation = null;
+        public float? value = null;
+    }
+    public class OopsPawnRenderingProps : PawnRenderNodeProperties
+    {
+
+        /// <summary>
+        /// Sets the lowest permitted value/saturation. (Hue does nothing).
+        /// Runs before the multiplier
+        /// </summary>
+        public HSVColor hsvClampMin = null; 
+        public HSVColor hsvClampMax = null;
+        public HSVColor hsvMultiplier = null;
+    }
+
     public class PawnRenderNode_FurSkinClr : PawnRenderNode_Fur
     {
         public PawnRenderNode_FurSkinClr(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
@@ -19,6 +37,69 @@ namespace OopsBug
         public override Color ColorFor(Pawn pawn)
         {
             return pawn.story.SkinColor;
+        }
+    }
+
+    public class PawnRenderNode_HSVTweak : PawnRenderNode
+    {
+        OopsPawnRenderingProps OopsProps => (OopsPawnRenderingProps)props;
+        public PawnRenderNode_HSVTweak(Pawn pawn, OopsPawnRenderingProps props, PawnRenderTree tree)
+            : base(pawn, props, tree)
+        {
+        }
+
+        public override Color ColorFor(Pawn pawn)
+        {
+            var baseClr = base.ColorFor(pawn);
+
+            // Brighten the color
+            Color.RGBToHSV(baseClr, out float h, out float s, out float v);
+
+            if (OopsProps.hsvClampMin != null )
+            {
+                var hsvC = OopsProps.hsvClampMin;
+                (float? hue, float? saturation, float? value) = (hsvC.hue, hsvC.saturation, hsvC.value);
+                if (value != null)
+                {
+                    v = Mathf.Max(value.Value, v);
+                }
+                if (saturation != null)
+                {
+                    s = Mathf.Max(saturation.Value, s);
+                }
+            }
+            if (OopsProps.hsvClampMax != null)
+            {
+                var hsvC = OopsProps.hsvClampMax;
+                (float? hue, float? saturation, float? value) = (hsvC.hue, hsvC.saturation, hsvC.value);
+                if (value != null)
+                {
+                    v = Mathf.Min(value.Value, v);
+                }
+                if (saturation != null)
+                {
+                    s = Mathf.Min(saturation.Value, s);
+                }
+            }
+
+            if (OopsProps.hsvMultiplier != null)
+            {
+                var hsvM = OopsProps.hsvMultiplier;
+                (float? hue, float? saturation, float? value) = (hsvM.hue, hsvM.saturation, hsvM.value);
+                if (hue != null)
+                {
+                    h = hue.Value;
+                }
+                if (value != null)
+                {
+                    v = Mathf.Max(0, Mathf.Min(1f, v * value.Value)); // 1.25f
+                }
+                if (saturation != null)
+                {
+                    s = Mathf.Max(0, Mathf.Min(1f, s * saturation.Value)); // 0.55f
+                }
+            }
+            return Color.HSVToRGB(h, s, v);
         }
     }
 
